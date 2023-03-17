30c4a4a94d7ea0bff8a85c146a2ec34a6b516890
ddo0ii <soyeong5857@gmail.com>
2023-03-17 18:02:12 +0900

    props.children 활용하기 - wrapper안에 main넣기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
 import Main from "./component/Main";
+import Wrapper from "./component/Wrapper";
 function App() {
   return (
     <div>
       <Header />
-      <Main name="권소영" color="blue" maleYn />
+      <Wrapper>
+        <Main color="blue" />
+      </Wrapper>
       <Footer />
     </div>
   );
