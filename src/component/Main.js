2a1ae4d7098d311686b3f7d1f0708d02e6f594e5
ddo0ii <soyeong5857@gmail.com>
2023-03-20 22:55:41 +0900

    onClick={() => setCnt((prevCnt) => prevCnt + 1)}

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 
 const Main = () => {
-  const [state, setState] = useState({ cnt: 0 });
-  const updateCnt = (val) =>
-    setState({
-      ...state,
-      [val]: state[val] + 1,
-    });
-  const { cnt } = state;
+  const [cnt, setCnt] = useState(0);
+  // const updateCnt = () => setCnt(cnt + 1);
+  // const clearCnt = () => setCnt(0);
   return (
     <div>
-      클릭한 횟수는 {cnt}번 입니다.
+      클릭한 횟수는 {cnt}번입니다.
       <div>
-        <button onClick={updateCnt.bind(null, "cnt")}>클릭해 보세요!</button>
+        <button onClick={() => setCnt((prevCnt) => prevCnt + 1)}>
+          클릭해 보세요!
+        </button>
+        <button onClick={() => setCnt(0)}>초기화 하기</button>
       </div>
     </div>
   );
