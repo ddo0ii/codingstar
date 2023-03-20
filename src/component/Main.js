388add6267827b872c478c54c38c6bc728994d40
ddo0ii <soyeong5857@gmail.com>
2023-03-20 16:07:46 +0900

    document.getElementById("name").innerText

--- src/component/Main.js
+++ src/component/Main.js
@@ -6,11 +6,14 @@ const Main = () => {
   function changeName() {
     myName = myName === "KwonSoYeong" ? "KimSoYeong" : "KwonSoYeong";
     console.log(myName);
+    document.getElementById("name").innerText = myName;
   }
 
   return (
     <div>
-      <h1>안녕하세요. {myName} 입니다.</h1>
+      <h1>
+        안녕하세요. <span id="name">{myName}</span> 입니다.
+      </h1>
       <button onClick={changeName}>Change</button>
     </div>
   );
