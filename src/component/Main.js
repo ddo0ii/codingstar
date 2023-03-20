28aea9ce0fccd5ff927830d4a10888578b66e516
ddo0ii <soyeong5857@gmail.com>
2023-03-20 16:12:31 +0900

    useState를 이용하여 변경

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Main = () => {
-  let myName = "KwonSoYeong";
+  // let myName = "KwonSoYeong"; // useState를 사용하여 변경
+  const [myName, setMyName] = useState("KwonSoYeong");
 
   function changeName() {
-    myName = myName === "KwonSoYeong" ? "KimSoYeong" : "KwonSoYeong";
-    console.log(myName);
-    document.getElementById("name").innerText = myName;
+    // myName = myName === "KwonSoYeong" ? "KimSoYeong" : "KwonSoYeong";
+    // console.log(myName);
+    // document.getElementById("name").innerText = myName;
+    setMyName(myName === "KwonSoYeong" ? "KimSoYeong" : "KwonSoYeong");
   }
 
   return (
     <div>
-      <h1>
-        안녕하세요. <span id="name">{myName}</span> 입니다.
-      </h1>
+      <h1>안녕하세요. {myName} 입니다.</h1>
       <button onClick={changeName}>Change</button>
     </div>
   );
