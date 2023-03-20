40d84dd20167d958d440e31fd8c48452291d2762
ddo0ii <soyeong5857@gmail.com>
2023-03-20 22:52:02 +0900

    카운터, state로 적용시켜보기 - bind

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
 const Main = () => {
-  const [cnt, setCnt] = useState(0);
-  const updateCnt = () => setCnt(cnt + 1);
-  const clearCnt = () => setCnt(0);
+  const [state, setState] = useState({ cnt: 0 });
+  const updateCnt = (val) =>
+    setState({
+      ...state,
+      [val]: state[val] + 1,
+    });
+  const { cnt } = state;
   return (
     <div>
       클릭한 횟수는 {cnt}번 입니다.
       <div>
-        <button onClick={updateCnt}>클릭해 보세요!</button>
-        <button onClick={clearCnt}>초기화 하기</button>
+        <button onClick={updateCnt.bind(null, "cnt")}>클릭해 보세요!</button>
       </div>
     </div>
   );
