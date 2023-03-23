a850810ee349643a0b72221e833435dcfb495220
ddo0ii <soyeong5857@gmail.com>
2023-03-23 15:36:38 +0900

    e.preventDefault를 명시적으로 호출하여 기본 링크 동작을 막을 수 있다.

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 
 const Main = () => {
   const [myName, setMyName] = useState("KwonSoYeong");
-  function changName() {
+  function changName(e) {
+    e.preventDefault();
     setMyName(myName === "KwonSoYeong" ? "KimSoYeong" : "KwonSoYeong");
   }
   return (
     <div>
       <h1>안녕하세요 {myName}</h1>
-      <a href="alert(1)" onClick={changName}>
+      <a href="#" onClick={changName}>
         Change
       </a>
     </div>
