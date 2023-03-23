d626664c6cb413361cf5132bd0b4d099b842f6cf
ddo0ii <soyeong5857@gmail.com>
2023-03-24 01:52:45 +0900

    CSS styling 작성, Component에서 css파일 import

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,13 +1,10 @@
 import React from "react";
+import "./Main.css";
 
 const Main = () => {
-  const styleObj = {
-    color: "yellow",
-    backgroundColor: "black",
-  };
   return (
     <>
-      <h3 style={styleObj}>안녕하세요. 권소영입니다.</h3>
+      <h3 className="nameBox">안녕하세요. 권소영입니다.</h3>
     </>
   );
 };
