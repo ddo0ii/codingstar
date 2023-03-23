a2da090521ad2443c11af67d44519adf8aa30af1
ddo0ii <soyeong5857@gmail.com>
2023-03-24 01:59:17 +0900

    ss-module

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ const App = () => {
   return (
     <div>
       <Main />
-      <h3 className="nameBox">안녕하세요. 권소영입니다.</h3>
     </div>
   );
 };
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Main.css";
+import styles from "./Main.module.css";
 
 const Main = () => {
   return (
     <>
-      <h3 className="nameBox">안녕하세요. 권소영입니다.</h3>
+      <h3 className={styles.nameBox}>안녕하세요. 권소영입니다.</h3>
     </>
   );
 };
