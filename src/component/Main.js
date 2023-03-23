845621b326a3d9b8dfaa8da5546bd3a82bb0a282
ddo0ii <soyeong5857@gmail.com>
2023-03-24 01:49:44 +0900

    CSS 인라인(inline) styling -  javascript object 이용

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Main = () => {
+  const styleObj = {
+    color: "yellow",
+    backgroundColor: "black",
+  };
   return (
     <>
-      <h3 style={{ color: "yellow", backgroundColor: "black" }}>
-        안녕하세요. 권소영입니다.
-      </h3>
+      <h3 style={styleObj}>안녕하세요. 권소영입니다.</h3>
     </>
   );
 };
