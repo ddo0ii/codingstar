90967853d831247645bd4f462495799ececa4bc2
ddo0ii <soyeong5857@gmail.com>
2023-03-23 23:46:26 +0900

    filter함수로 제한주기!

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Main from "./component/Main";
 const App = () => {
   const names = [
     { userName: "권소영", age: 19 },
-    { userName: "김소영", age: 17 },
-    { userName: "나소영", age: 12 },
+    { userName: "김소영", age: 28 },
+    { userName: "나소영", age: 36 },
   ];
-  const nameList = names.map((v) => <Main name={v.userName} age={v.age} />);
+  // const로 선언하면안된다! >> let으로 선언하자!
+  let nameList = names.filter((v) => v.age < 30);
+  nameList = nameList.map((v) => <Main name={v.userName} age={v.age} />);
+
   return <div>{nameList}</div>;
 };
 
