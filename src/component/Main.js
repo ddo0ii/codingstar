a5d3d7032f7b44cb8df6e0f29c2551fb631bc9b9
ddo0ii <soyeong5857@gmail.com>
2023-03-23 11:38:54 +0900

    함수 직접 선언하여 사용 >> onClick

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 
 const Main = () => {
   const [myName, setMyName] = useState("KwonSoYeong");
-  function changeName() {
-    setMyName(myName === "KwonSoYeong" ? "KimSoYeong" : "KwonSoYeong");
-  }
   return (
     <div>
       <h1>안녕하세요 {myName}</h1>
-      <button onClick={changeName}>Change</button>
+      <button
+        onClick={() => {
+          setMyName(myName === "KwonSoYeong" ? "KimSoYeong" : "KwonSoYeong");
+        }}
+      >
+        Change
+      </button>
     </div>
   );
 };
