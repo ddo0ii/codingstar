fb22c3fd39df810259fd270c462e528810fd241b
ddo0ii <soyeong5857@gmail.com>
2023-03-24 02:01:16 +0900

    css-module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Main from "./component/Main";
+import styles from "./App.module.css";
 
 const App = () => {
   return (
     <div>
       <Main />
+      <h3 className={styles.nameBox}>안녕하세요. 권소영입니다.</h3>
     </div>
   );
 };
