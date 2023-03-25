971b700e2582546228a52298c6d5ef7d1bf8f08e
ddo0ii <soyeong5857@gmail.com>
2023-03-25 18:38:26 +0900

    cleanup 함수

--- src/hooks/UseEffectTest.js
+++ src/hooks/UseEffectTest.js
@@ -1,3 +1,4 @@
+import { cleanup } from "@testing-library/react";
 import React, { useEffect, useState } from "react";
 
 function UseEffectTest() {
@@ -8,6 +9,13 @@ function UseEffectTest() {
 
   useEffect(() => {
     console.log("useEffect!!", count);
+    return () => {
+      // cleanup 함수
+      // useEffect 안에서 return 할 때 실행 된다.(useEffect의 뒷정리를 한다.)
+      // 만약 컴포넌트가 마운트 될 때 이벤트 리스너를 통해 이벤트를 추가하였다면 컴포넌트가 언마운트 될 때 이벤트를 삭제 해주어야 한다.
+      // 그렇지 않으면 컴포넌트가 리렌더링 될 때마다 새로운 이벤트 리스너가 핸들러에 바인딩 될 것이다. 이는 자주 리렌더링 될 경우 메모리 누수가 발생할 수 있다.
+      console.log("cleanup!!", count);
+    };
   }, [count]);
 
   return (
