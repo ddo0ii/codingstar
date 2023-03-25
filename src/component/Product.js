59132f3c553ebb8d42894c97a4cf96e2a8fbd0be
ddo0ii <soyeong5857@gmail.com>
2023-03-25 17:41:21 +0900

    useNavigate - Link 없이 이동, replace 옵션- 히스토리 남기지않음

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,18 +1,35 @@
 import React from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
-// sample url : http://localhost:3000/product/1?search=productName&q=demo#test
 const Product = () => {
   const productId = useParams().productId;
-  const [searchParams, setSearchParams] = useSearchParams();
-  const keyWords = searchParams; // 얘는 에러남..ㅠ
-  const keyWord = searchParams.get("search");
+  const navigate = useNavigate();
+  // Link 컴포넌트를 사용하지 않고 다른 페이지로 이동을 해야 하는 경우, 뒤로가기 등에 사용하는 Hook 이다.
+  // replace 옵션을 사용하면 페이지를 이동할 때 히스토리를 남기지 않는다.
   return (
     <>
       <h3>{productId}번 상품 페이지 입니다.</h3>
       <ul>
-        <li>keyWords: {keyWords}</li>
-        <li>keyWord: {keyWord}</li>
+        <li>
+          <button onClick={() => navigate(-2)}>Go 2 pages back</button>
+        </li>
+        <li>
+          <button onClick={() => navigate(-1)}>Go back</button>
+        </li>
+        <li>
+          <button onClick={() => navigate(1)}>Go forward</button>
+        </li>
+        <li>
+          <button onClick={() => navigate(2)}>Go 2 pages forward</button>
+        </li>
+        <li>
+          <button onClick={() => navigate("/")}>Go Root</button>
+        </li>
+        <li>
+          <button onClick={() => navigate("/", { replace: true })}>
+            Go Root{" "}
+          </button>
+        </li>
       </ul>
     </>
   );
