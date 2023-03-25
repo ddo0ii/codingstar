bcff4aad398cf197bfcd04da1967f7b2c654f2bd
ddo0ii <soyeong5857@gmail.com>
2023-03-25 17:01:29 +0900

    useLocation, useSearchParams 사용

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 const Product = () => {
-  const { productId } = useParams();
+  const productId = useParams().productId;
+  const location = useLocation();
+
   return (
     <>
-      <h3>{productId}번 상품 페이지입니다.</h3>
+      <h3>{productId}번 상품 페이지 입니다.</h3>
+      <ul>
+        <li>hash : {location.hash}</li>
+        <li>pathname : {location.pathname}</li>
+        <li>search : {location.search}</li>
+        <li>state : {location.state}</li>
+        <li>key : {location.key}</li>
+      </ul>
     </>
   );
 };
