116a56327b53a8873b15d10574a156388e18c0b7
ddo0ii <soyeong5857@gmail.com>
2023-03-25 18:28:33 +0900

    useEffect 특정 값이 변경될 때에만 실행하는 경우

--- src/hooks/UseEffectTest.js
+++ src/hooks/UseEffectTest.js
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from "react";
 
 function UseEffectTest() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("KwonSoYeong");
   const countUp = () => setCount(count + 1);
+  const handleChangeName = (e) => setName(e.target.value);
 
   useEffect(() => {
     console.log("useEffect!!", count);
-  }, []);
+  }, [count]);
+
   return (
     <div>
+      <p>안녕하세요. {name} 입니다.</p>
+      <input onChange={handleChangeName} />
       <p>{count}번 클릭!</p>
       <button onClick={countUp}>Click Me</button>
     </div>
